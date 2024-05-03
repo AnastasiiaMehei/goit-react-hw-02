b501c9592aa5a5eefaa962bc39856aef953efb8e
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-05-03 15:46:22 +0300

    AD

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './App.module.css'
 import Description from '../Description/Description'
 import Options from '../Options/Options'
 import Feedback from '../Feedback/Feedback'
 import Notification from '../Notification/Notification'
 export default function App() {
-  const [values, setValues] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
+  const [values, setValues] = useState(() => {
+    const savedValues = window.localStorage.getItem("saved-values");
+if (savedValues !== null) {
+  return JSON.parse(savedValues)
+}
+return {
+  good: 0,
+  neutral: 0,
+  bad: 0
+};
+}  );
+  useEffect(() => {
+    window.localStorage.setItem("saved-values", JSON.stringify({values}));
+  }, [values]); 
+  
   const updateFeedback = (feedbackType) => {
     setValues(prevValues => ({
        ...prevValues,
@@ -25,6 +35,7 @@ const resetFeedbacks = () => {
 };
   const totalFeedback = values.good + values.neutral + values.bad;
   const positiveFeedbacks = totalFeedback > 0? Math.round((values.good / totalFeedback) * 100):0; 
+ 
   return (
     <>
      <Description />
